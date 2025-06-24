32f667b00ae07d0fb58350df5cd84a47cd95928b
Bodgar <b.kwizera@alustudent.com>
2025-06-24 06:43:17 -0700

    Add 8-uppercase.py: prints string in uppercase without str.upper()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for c in str:
         if 97 <= ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            result += "{}".format(chr(ord(c) - 32))
         else:
-            print("{}".format(c), end="")
-    print()
+            result += "{}".format(c)
+    print(result)
