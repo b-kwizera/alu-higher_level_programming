9076215393394903eccc818dee829417dbb82064
Bodgar <b.kwizera@alustudent.com>
2025-07-04 07:26:23 -0700

    Add inherits_from function that checks class inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""Defines a function to check if an object is an instance of a subclass of a class."""
+"""
+This module provides a function to check if an object is an instance of
+a class that inherited (directly or indirectly) from the specified class.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherits from a_class.
+    """
+    Check if an object is an instance of a class that inherited from the specified class.
+
+    This function returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class, but not if
+    the object is an instance of the specified class itself.
 
     Args:
-        obj: The object to check.
-        a_class: The class to check inheritance from.
+        obj: The object to check
+        a_class: The class to check inheritance from
 
     Returns:
-        True if obj is instance of a subclass of a_class (not a_class itself), else False.
+        bool: True if obj's class inherits from a_class (but is not a_class itself),
+              False otherwise
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
