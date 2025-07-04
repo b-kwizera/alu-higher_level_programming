30ffde64df757877da1e3e1fa791978cc17f92ef
Bodgar <b.kwizera@alustudent.com>
2025-07-04 09:58:02 -0700

    Removing whitespaces

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represents a square with a private size attribute."""
-    
+
     def __init__(self, size):
         """Initializes the square with a given size (no validation)."""
         self.__size = size
