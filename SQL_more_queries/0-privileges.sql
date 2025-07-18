1bbf4363dff6d0490c34fdb2e1de6e2593b01a2e
Bodgar <b.kwizera@alustudent.com>
2025-07-18 07:04:15 -0700

    Add script to show privileges of user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- Lists all privileges of user_0d_1 and user_0d_2 on localhost
-
+-- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
