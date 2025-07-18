82bc29a5521a49eb061a7e60f4e872600dcf806e
Bodgar <b.kwizera@alustudent.com>
2025-07-18 07:57:04 -0700

    List all TV shows in genre Comedy sorted by title

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- List all TV shows that belong to the genre 'Comedy', sorted by show title
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
