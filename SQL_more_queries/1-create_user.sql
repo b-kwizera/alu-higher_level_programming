20b2678d492b7767a3894622896cc24952f0dbec
Bodgar <b.kwizera@alustudent.com>
2025-07-18 06:56:05 -0700

    Add script to create user_0d_1 with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create user_0d_1 with full privileges if not exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
