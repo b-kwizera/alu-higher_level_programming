ebae8f86598cac0f48acfab5b9d9040a1c1ba07c
Bodgar <b.kwizera@alustudent.com>
2025-07-18 05:20:17 -0700

    Add 8-class_to_json.py: returns dictionary description for JSON serialization

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ Function to return the dictionary description with simple data structure
 for JSON serialization of an object.
 """
 
+
 def class_to_json(obj):
     """
     Returns the dictionary description with simple data structure
@@ -14,6 +15,6 @@ def class_to_json(obj):
         obj (object): An instance of a class
 
     Returns:
-        dict: Dictionary of the object's attributes suitable for JSON serialization
+        dict: Dictionary of the object's attributes suitable
     """
     return obj.__dict__.copy()
