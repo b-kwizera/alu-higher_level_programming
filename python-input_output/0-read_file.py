4871f741b48b27fc4fd968b0540f479bd345ebda
Bodgar <b.kwizera@alustudent.com>
2025-07-18 04:54:56 -0700

    Add 0-read_file.py: function to read and print file content

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module provides a function to read and print the contents of a text file."""
+"""This module provides a function to read."""
 
 
 def read_file(filename=""):
