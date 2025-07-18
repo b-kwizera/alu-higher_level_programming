154ba0b69d982cb2525d358ad7ba0fb85dc6fe30
Bodgar <b.kwizera@alustudent.com>
2025-07-18 04:57:22 -0700

    Add 1-write_file.py: function to write a string to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module provides a function to write text to a file."""
+
+
+def write_file(filename="", text=""):
+     """Writes a string to a UTF-8 text file ."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
