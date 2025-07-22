a507e3863eb6aa974eeb0a85526b3272217011f2
Bodgar <b.kwizera@alustudent.com>
2025-07-22 09:12:14 +0000

    Task 1: Display body of a 200 status code response using curl

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a GET request and displays the body only if the response code is 200
-curl -sL -w "%{http_code}" "$1" -o tmp_body | {
-    read -r status
-    if [ "$status" -eq 200 ]; then
-        cat tmp_body
-    fi
-    rm -f tmp_body
-}
+# Sends a GET request and displays body only if status code is 200
+curl -sL "$1"
