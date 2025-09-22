80d9148855d11300fd627b5345f622eff386e8a4
Bodgar <b.kwizera@alustudent.com>
2025-09-22 04:05:54 -0700

    Create 1-rectangle.js with Rectangle class constructor

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Class Rectangle with constructor initializing width and height
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
