56f7054905ad33844ea0a7fc5c7704913393f0ad
Bodgar <b.kwizera@alustudent.com>
2025-09-22 03:50:06 -0700

    Create 13-add.js with add function returning sum of two integers

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Function that returns the addition of 2 integers
+function add(a, b) {
+  return a + b;
+}
+
+// Export the function so it's visible from outside
+exports.add = add;
