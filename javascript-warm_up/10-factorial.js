65568809ef9b80b1ab78f5f410e5f04fb3b4f49b
Bodgar <b.kwizera@alustudent.com>
2025-09-22 03:38:16 -0700

    Add 10-factorial.js script to compute and print a factorial recursively

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function factorial(n) {
+  const num = parseInt(n);
+  if (isNaN(num) || num <= 1) return 1;
+  return num * factorial(num - 1);
+}
+console.log(factorial(process.argv[2]));
